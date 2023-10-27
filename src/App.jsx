36f7f212220efb3ec74bbe530d49348e598fe218
Mehul <mehulbohra11@gmail.com>
2023-10-28 00:46:06 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div className="flex border border-red-500 flex-col p-8 justify-center items-center h-max gap-5 rounded-xl">
         <h1 className="sm:text-5xl text-diffBlack text-3xl font-mono">Enter your Youtube video URL :- </h1>
-        <form action="https://0f08-2405-201-5c24-50dc-1940-e73d-4aab-885.ngrok-free.app" method='POST' className="flex flex-col w-full justify-center items-center gap-4 h-full">
+        <form action="http://localhost:3000/api/send" method='POST' className="flex flex-col w-full justify-center items-center gap-4 h-full">
         <input type="text"  className={`px-6 py-4 text-2xl text-gray-300 font-semibold w-full h-full max-w-2xl rounded-lg focus:outline-red-800 bg-diffBlack backdrop-blur-md`} name='url' onClick={handleClick}/>
         <button type='submit' className='px-5 py-3 text-2xl rounded-md max-w-md text-white bg-orange-700 active:scale-95'>Download</button>
         </form>
