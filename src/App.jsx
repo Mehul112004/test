15329263006f60f340731dd1f9c744330d063eda
Mehul <mehulbohra11@gmail.com>
2023-11-05 23:44:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div className="flex border border-red-500 flex-col p-8 justify-center items-center h-max gap-5 rounded-xl">
         <h1 className="sm:text-5xl text-diffBlack text-3xl font-mono">Enter your Youtube video URL :- </h1>
-        <form action="https://yt-backend-xq0k.onrender.com//api/send" method='POST' className="flex flex-col w-full justify-center items-center gap-4 h-full">
+        <form action="https://yt-backend-xq0k.onrender.com/api/send" method='POST' className="flex flex-col w-full justify-center items-center gap-4 h-full">
         <input type="text" placeholder="URL...." className={`px-6 py-4 text-2xl text-gray-300 font-semibold w-full h-full max-w-2xl rounded-lg focus:outline-red-800 bg-diffBlack backdrop-blur-md`} name='url' onClick={handleClick}/>
         <button type='submit' className='px-5 py-3 text-2xl rounded-md max-w-md text-white bg-orange-700 active:scale-95'>Download</button>
         </form>
